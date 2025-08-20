17811b3b6b52cdf33a9dafc81e3355bedd6d0baa
Gabriel <bielrj170@gmail.com>
2025-08-20 02:35:14 -0300

    java

---
+++ src/j202_variaveis/VariaveisApp.java
@@ -0,0 +1,17 @@
+package j202_variaveis;
+
+public class VariaveisApp {
+    public static void main(String[] args) {
+        final String BR = "Brasil";
+        int idade = 13;
+        double saldo = 0.0;
+        boolean isGreatherThan18 = true;
+
+
+        System.out.println(BR);
+        System.out.println(idade);
+        System.out.println(saldo);
+        System.out.println(isGreatherThan18);
+    }
+}
+
