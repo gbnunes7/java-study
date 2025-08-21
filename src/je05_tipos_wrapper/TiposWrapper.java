cfda7bc7c85f4e6be6ce02436fe18d47e63f838e
Gabriel <bielrj170@gmail.com>
2025-08-20 22:48:56 -0300

    java

---
+++ src/je05_tipos_wrapper/TiposWrapper.java
@@ -0,0 +1,10 @@
+package je05_tipos_wrapper;
+
+public class TiposWrapper {
+    public static void main(String[] args) {
+        Double numero = 3.87;
+        Integer numeroInt = numero.intValue();
+        System.out.println(numeroInt);
+        
+    }
+}
